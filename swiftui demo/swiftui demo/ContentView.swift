b3d078d28601f2a38da8aeea1c22c3cecd19a3df
mddd <413153189@qq.com>
2022-06-02 13:36:41 +0800

    学习swiftui

--- swiftui demo/swiftui demo/ContentView.swift
+++ swiftui demo/swiftui demo/ContentView.swift
@@ -21,6 +21,9 @@ struct ContentView: View {
             Text("SwiftUI for ios 15")
                 .font(.largeTitle)
             .fontWeight(.bold)
+            .foregroundStyle(.linearGradient(colors: [.primary,
+                                                      .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
+            .lineLimit(1)
             
             Text("20 sectioins - 3 hours".uppercased())
                 .font(.footnote)
@@ -37,11 +40,21 @@ struct ContentView: View {
         .padding(.all, 20.0)
         .padding(.vertical,20)
         .frame(height: 350.0)
-        .background(Color("Background"))
+        .background(.ultraThinMaterial)
         .cornerRadius(30.0)
         .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 10, y: 10)
         .padding(.horizontal,20)
-        
+        .background(
+            Image("Blob 1")
+            .offset(x: 250,y: -100)
+        )
+        .overlay(
+        Image("Illustration 5")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(height: 230)
+            .offset(x: 32, y: -80)
+        )
           
     }
 }
@@ -49,5 +62,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
+            
     }
 }
