ebb6adb751abf6fc56822d722c9c41f25ac4c922
mddd <413153189@qq.com>
2022-06-09 11:05:03 +0800

    修改卡片样式

--- swiftui demo/swiftui demo/ContentView.swift
+++ swiftui demo/swiftui demo/ContentView.swift
@@ -12,50 +12,53 @@ struct ContentView: View {
         VStack(alignment: .leading, spacing: 8.0) {
             Spacer()
             Image("Logo 2")
-                .resizable( resizingMode: .stretch)
+                .resizable(resizingMode: .stretch)
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 26.0, height: 26.0)
                 .cornerRadius(20.0)
                 .padding()
+
             
             Text("SwiftUI for ios 15")
                 .font(.largeTitle)
-            .fontWeight(.bold)
-            .foregroundStyle(.linearGradient(colors: [.primary,
-                                                      .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
-            .lineLimit(1)
-            
+                .fontWeight(.bold)
+                .foregroundStyle(.linearGradient(colors: [.primary,
+                                                          .primary.opacity(0.5)], startPoint: .topLeading, endPoint: .bottomTrailing))
+                .lineLimit(1)
+
             Text("20 sectioins - 3 hours".uppercased())
                 .font(.footnote)
                 .fontWeight(.semibold)
                 .foregroundColor(.secondary)
-            
+
             Text("Build an IOS app for IOS 15 with custom layouts, animations and ...")
                 .font(.footnote)
                 .multilineTextAlignment(.leading)
                 .lineLimit(2)
-                .frame(maxWidth: .infinity,alignment: .leading)
+                .frame(maxWidth: .infinity, alignment: .leading)
                 .foregroundColor(.secondary)
+        
+            
         }
         .padding(.all, 20.0)
-        .padding(.vertical,20)
+        .padding(.vertical, 20)
         .frame(height: 350.0)
-        .background(.ultraThinMaterial)
-        .cornerRadius(30.0)
+        .background(.ultraThinMaterial,in: RoundedRectangle(cornerRadius: 30.0,style: .continuous))
+//        .cornerRadius(30.0)
+       // .mask(RoundedRectangle(cornerRadius: 30.0,style: .continuous)) // Ellipse() 椭圆 Rectangle 矩形 Capsule 胶囊
         .shadow(color: Color("Shadow").opacity(0.3), radius: 10, x: 10, y: 10)
-        .padding(.horizontal,20)
+        .padding(.horizontal, 20)
         .background(
             Image("Blob 1")
-            .offset(x: 250,y: -100)
+                .offset(x: 250, y: -100)
         )
         .overlay(
-        Image("Illustration 5")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            .frame(height: 230)
-            .offset(x: 32, y: -80)
+            Image("Illustration 5")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(height: 230)
+                .offset(x: 32, y: -80)
         )
-          
     }
 }
 
@@ -63,6 +66,5 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
             .preferredColorScheme(.light)
-            
     }
 }
